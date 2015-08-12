99a4b6c818c904cab4502a773b6504bbb45941f3
Pixels-addict <contact@pixels-addict.fr>
2015-08-12 04:08:50 +0200

    Update popup-generator.php

--- popup-generator.php
+++ popup-generator.php
@@ -14,36 +14,40 @@ define( 'ME_DIR', plugin_dir_path( __FILE__ ) );
 define( 'ME_VERSION', '0.1' );
 define( 'ME_OPTION', 'me_ext' );
 
+// Register and enqueue popup's javascript
+
 function popup_generator(){
-if (!is_admin()) {
-wp_register_script('popup', ME_JS_DIR . 'jquery.popup.js');
-wp_enqueue_script('popup');
+        if (!is_admin()) {
+                wp_register_script('popup', ME_JS_DIR . 'jquery.popup.js');
+                wp_enqueue_script('popup');
+        }
 }
-}
+
+// Deregister WordPress jQuery and load jQuery directly from Google libraries 
 
 function google_jquery() {
-if (!is_admin()) {
-wp_deregister_script('jquery');
-wp_register_script('jquery','http://ajax.googleapis.com/ajax/libs/jquery/1/
+        if (!is_admin()) {
+            wp_deregister_script('jquery');
+            wp_register_script('jquery','http://ajax.googleapis.com/ajax/libs/jquery/1/
 jquery.min.js', false, '');
-wp_enqueue_script('jquery');
-wp_deregister_script('jquery-ui');
-wp_register_script('jquery-ui','http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/
+            wp_enqueue_script('jquery');
+            wp_deregister_script('jquery-ui');
+            wp_register_script('jquery-ui','http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/
 jquery-ui.min.js', false, '');
-wp_enqueue_script('jquery-ui');
+            wp_enqueue_script('jquery-ui');
+        }
 }
-}
+
 add_action('init', 'google_jquery');
 
-function add_stylesheets()
-{
-    
+// Register and enqueue the popup's stylesheet 
+
+function add_stylesheets() {
     wp_register_style( 'popup_style',
     ME_CSS_DIR . 'popup-style.css',
     array(),
     ME_VERSION,
     'all' );
-
     wp_enqueue_style( 'popup_style' );
 }
 
